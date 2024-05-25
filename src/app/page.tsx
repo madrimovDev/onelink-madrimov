775370a9dabf9941c0d239fe3f9d8c5f12d1a2d1
Madrimov <madrimov5014@gmail.com>
2024-05-25 11:04:43 +0500

    initial

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,197 @@
+import { Metadata } from "next";
 import Image from "next/image";
+import Link from "next/link";
+export const metadata: Metadata = {
+	title: "Madrimov Dev - Full Stack Developer",
+	description:
+		"Madrimov Dev - Full Stack Developer. Frontend Developer with experience in ReactJS, TypeScript, Redux Toolkit, and more. Portfolio at madrimov.uz.",
+	keywords: [
+		"Full Stack Developer",
+		"Frontend Developer",
+		"ReactJS",
+		"TypeScript",
+		"Redux Toolkit",
+		"NextJS",
+	],
+	authors: [
+		{
+			name: "Madrimov Xudoshukur",
+			url: "https://madrimov.uz",
+		},
+	],
+	applicationName: "Madrimaov's One-link",
+	category: "Programming",
+	creator: "Madrimov Xudoshukur",
+	bookmarks: [
+		"https://t.me/madrimov",
+		"https://instagram.com/xudish.madrimov",
+		"https://github.com/madrimovDev",
+	],
+	formatDetection: { email: true, date: true, telephone: true, url: true },
+	classification: "Programming",
+	publisher: "Vercel",
+	openGraph: {
+		type: "website",
+		countryName: "Uzbekistan",
+		description:
+			"Madrimov Dev - Full Stack Developer. Frontend Developer with experience in ReactJS, TypeScript, Redux Toolkit, and more. Portfolio at madrimov.uz",
+		emails: "madrimov5014@gmail.com",
+		phoneNumbers: "+998914245014",
+		siteName: "onelink.madrimov.uz",
+		title: "Madrimov Xudoshukur",
+		url: "https://onelink.madrimov.uz",
+		images: [
+			{
+				url: "https://www.madrimov.uz/avatar.jpg",
+				alt: "Madrimov Xudoshukur Portfolio",
+			},
+		],
+	},
+	twitter: {
+		creatorId: "@madrimov_x",
+		site: "@madrimov_x",
+		card: "summary_large_image",
+	},
+	robots: {
+		index: true,
+		follow: true,
+		nocache: false,
+		googleBot: {
+			index: true,
+			follow: true,
+			noimageindex: true,
+		},
+	},
+};
 
-export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+export default function Page() {
+	return (
+		<div className="min-h-screen flex flex-col">
+			<video
+				autoPlay
+				muted
+				loop
+				className="fixed w-screen h-screen scale-110 z-[-1] opacity-30 blur-sm object-cover"
+			>
+				<source
+					src="/background.webm"
+					type="video/webm"
+					width="100%"
+				/>
+			</video>
+			<div className="py-10 px-4 max-w-lg w-full flex-1 flex flex-col justify-center mx-auto">
+				<div className="flex items-center justify-between gap-2">
+					<div>
+						<h1 className="text-2xl font-bold">Madrimov Xudoshukur</h1>
+						<h2 className="mt-2 text-gray-400">Fullstack Developer</h2>
+					</div>
+					<Image
+						src="https://madrimov.uz/avatar.jpg"
+						width={100}
+						height={100}
+						alt="Madrimov Xudoshukur"
+						quality={50}
+						className="h-full aspect-square rounded-full"
+					/>
+				</div>
+				<div>
+					<p className="mt-4">
+						<b>email: </b>madrimov5014@gmail.com
+					</p>
+					<p>
+						<b>phone: </b>+998 91 424 50 14
+					</p>
+				</div>
+				<hr className="border-gray-500 my-8" />
+				<div className="flex flex-col gap-2">
+					<Link
+						href="https://github.com/madrimovDev"
+						className="flex items-center gap-4 bg-gray-800/50 hover:bg-gray-800 p-4 rounded-md hover:scale-105 hover:shadow-2xl transition-all"
+						target="_blank"
+					>
+						<Image
+							src="/github.svg"
+							width={35}
+							height={35}
+							alt="github"
+							className="bg-slate-200 rounded-full"
+						/>
+						<h4>github.com/madrimovDev</h4>
+					</Link>
+					<Link
+						href="https://www.linkedin.com/in/madrimov"
+						className="flex items-center gap-4 bg-gray-800/50 hover:bg-gray-800 p-4 rounded-md hover:scale-105 hover:shadow-2xl transition-all"
+						target="_blank"
+					>
+						<Image
+							src="/linkedin.svg"
+							width={35}
+							height={35}
+							alt="github"
+							className=""
+						/>
+						<h4>Xudoshukur Madrimov</h4>
+					</Link>
+					<Link
+						href="https://t.me/madrimov"
+						className="flex items-center gap-4 bg-gray-800/50 hover:bg-gray-800 p-4 rounded-md hover:scale-105 hover:shadow-2xl transition-all"
+						target="_blank"
+					>
+						<Image
+							src="/telegram.svg"
+							width={35}
+							height={35}
+							alt="github"
+							className="bg-slate-200 rounded-full"
+						/>
+						<h4>@madrimov</h4>
+					</Link>
+					<Link
+						href="https://instagram.com/xudish.madrimov"
+						className="flex items-center gap-4 bg-gray-800/50 hover:bg-gray-800 p-4 rounded-md hover:scale-105 hover:shadow-2xl transition-all"
+						target="_blank"
+					>
+						<Image
+							src="/instagram.svg"
+							width={35}
+							height={35}
+							alt="github"
+							className=""
+						/>
+						<h4>@xudish.madrimov</h4>
+					</Link>
 
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  );
+					<Link
+						href="https://madrimov.uz"
+						className="flex items-center gap-4 bg-gray-800/50 hover:bg-gray-800 p-4 rounded-md hover:scale-105 hover:shadow-2xl transition-all"
+						target="_blank"
+					>
+						<Image
+							src="/web.png"
+							width={35}
+							height={35}
+							alt="github"
+							className="bg-slate-200 rounded-full"
+						/>
+						<h4>madrimov.uz</h4>
+					</Link>
+					<Link
+						href="https://blog.madrimov.uz"
+						className="flex items-center gap-4 bg-gray-800/50 hover:bg-gray-800 p-4 rounded-md hover:scale-105 hover:shadow-2xl transition-all"
+						target="_blank"
+					>
+						<Image
+							src="/blog.png"
+							width={35}
+							height={35}
+							alt="github"
+							className="bg-slate-200 rounded-md"
+						/>
+						<h4>blog.madrimov.uz</h4>
+					</Link>
+				</div>
+			</div>
+		</div>
+	);
 }
+
